68cf08160f6314a9b891de47c7f97c928ab9538d
Sanyam105678 <96380392+Sanyam105678@users.noreply.github.com>
2022-04-14 15:00:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package com.masai;
+
+import java.util.Scanner;
+
+public class Demo {
+	
+	static void sum(int n) {
+		System.out.println(n*(n+1)/2);
+	}
+		 
+	public static void main(String[] args) {
+		
+		Demo.sum(10);
+	}
+
+}
